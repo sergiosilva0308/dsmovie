f71cc80e68067f75e42868e63fb0359477903d80
sergiosilva0308 <sergiosiilva@hotmail.com>
2022-01-11 14:35:13 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dvmovie/DvmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dvmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvmovieApplication.class, args);
+	}
+
+}
